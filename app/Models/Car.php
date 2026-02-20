8e6e7a7fcbe11de4457c35c8c0751d119676ddfe
samyelsayed <samyelsayed754@gmail.com>
2026-02-20 23:41:53 +0200

    Add SoftDeletes and fillable properties to User and Car models

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Car extends Model
-{
-    //
+{use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'plate_number',
+        'brand',
+        'model',
+        'year',
+        'color',
+    ];
+
+    
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes; 
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable ,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +19,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',  
+        'last_name',
         'email',
         'password',
+        'role',
     ];
 
     /**
